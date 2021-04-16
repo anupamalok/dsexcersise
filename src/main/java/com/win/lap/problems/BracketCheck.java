fdf9f688f52afc36689c36b4d4935508c0ca2109
Alok <Anupam_Alok@Dell.com>
2021-04-16 17:35:41 +0530

    Bracket problem

--- src/main/java/com/win/lap/problems/BracketCheck.java
+++ src/main/java/com/win/lap/problems/BracketCheck.java
@@ -1,5 +1,43 @@
 package com.win.lap.problems;
 
-public class BracketCheck {
+import java.util.Stack;
 
+public class BracketCheck {
+	
+	/**
+     * Input  --> Response
+     * "{}{}" --> Symmetric
+     * "{{}}" --> Symmetric
+     * "{}{{}}" --> Symmetric
+     * "}{{}" --> Asymmetric
+     * "}{" --> Asymmetric
+     * "{{" --> Asymmetric
+     * "{}{" --> Asymmetric
+     */
+	
+	public static void main(String args[]){
+		System.out.println(checkBracket("{}{"));
+	}
+
+	
+	static String checkBracket(String b){
+		Stack<Character> brack=new Stack<>();
+		char[] c = b.toCharArray();
+		for(int i=0;i<c.length;i++){
+		if(c[i] == '{' ){
+			brack.push('{');
+		}else{
+			if(!brack.empty()){
+			brack.pop();
+			}
+			}
+		}
+		
+		if(brack.size() == 0){
+			return "Symmetric";
+		}
+		else
+			return "ASymmetric";
+					
+	}
 }
